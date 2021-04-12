bbe35bd5c66525b6bbe283984938756a8aca9207
Ryan Elian <ryan.elian.cs@gmail.com>
2021-04-13 02:25:29 +0700

    Move Page interface to /components folder

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Page } from '../src/js/Page';
+import { Page } from '../src/js/components/Page';
 import { DefaultLayout } from '../src/js/layout/DefautLayout';
 import { Title } from '../src/js/components/Title';
 

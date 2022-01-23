f5d451c806202a42cb402266caa0208befb00125
Ryan Elian <ryan.elian.cs@gmail.com>
2022-01-23 22:39:06 +0700

    Simplify index page.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 import { Page } from '../src/js/types/Page';
 import { DefaultLayout } from '../src/js/layout/DefautLayout';
 import { Title } from '../src/js/components/Title';
-import Link from 'next/link';
 
 const Index: Page = () => {
     return (
         <div>
             <Title name="Home"></Title>
             Hello World!
-            <Link href="/about">
-                <a>About Page</a>
-            </Link>
         </div>
     );
 }

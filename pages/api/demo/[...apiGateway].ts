8b179c1892627697c61222c370531629290e6c1d
Ryan Elian <ryan.elian.cs@gmail.com>
2022-04-21 17:49:35 +0700

    Update packages, develop new API Gateway

--- pages/api/demo/[...apiGateway].ts
+++ pages/api/demo/[...apiGateway].ts
@@ -1,28 +1,31 @@
-import { createProxyMiddleware, Options } from 'http-proxy-middleware';
+import Proxy from 'http-proxy';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import { AppSettings } from '../../../functions/AppSettings';
 
-// Great way to avoid using CORS and to transparently add custom headers to API requests
-// Proxy requests from /api/demo/... to http://localhost:5000/...
-const apiProxyOptions: Options = {
+// Great way to avoid using CORS and making API calls from HTTPS pages to back-end HTTP servers
+const server = Proxy.createProxyServer({
     target: AppSettings.current.backendHost,
     changeOrigin: true,
-    // Set ws to true if proxying WebSocket
-    // Recommendation: create a dedicated API gateway for WebSocket, don't mix!
-    // If troublesome, just hit the WebSocket endpoint directly (CORS)
-    ws: false,
     xfwd: true,
-    pathRewrite: {
-        '^/api/demo': '/'
-    },
-    logLevel: 'warn'
-};
+    // https://github.com/http-party/node-http-proxy#proxying-websockets
+    ws: false,
+});
 
-const apiProxy = createProxyMiddleware(apiProxyOptions);
+server.on('proxyReq', (proxyReq, req) => {
+    // Proxy requests from /api/demo/... to http://my-web-api.com/...
+    const urlRewrite = req.url?.replace(new RegExp('^/api/demo'), '');
+    if (urlRewrite) {
+        proxyReq.path = urlRewrite;
+    } else {
+        proxyReq.path = '/';
+    }
+    // console.log('Proxying:', req.url, '-->', AppSettings.current.backendHost + urlRewrite);
+});
 
-const apiGateway = async (req, res) => {
-    apiProxy(req, res, (result: unknown) => {
-        if (result instanceof Error) {
-            throw result;
+const apiGateway = async (req: NextApiRequest, res: NextApiResponse) => {
+    server.web(req, res, {}, (err) => {
+        if (err instanceof Error) {
+            throw err;
         }
 
         throw new Error(`Failed to proxy request: '${req.url}'`);

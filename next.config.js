519f203d66530ef51cf41fbdc8b34d5efc36b9cc
Ryan Elian <ryan.elian.cs@gmail.com>
2022-10-07 13:46:54 +0700

    Add security headers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,47 @@
+function getSecurityHeaders(isProd) {
+    const headers = [
+        {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+        },
+        {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+        },
+        {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin'
+        },
+        {
+            // https://www.permissionspolicy.com/
+            key: 'Permissions-Policy',
+            value: 'accelerometer=(), autoplay=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()'
+        },
+        {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block'
+        }
+    ];
+
+    if (isProd) {
+        headers.push(
+            {
+                key: 'Strict-Transport-Security',
+                value: 'max-age=31536000'
+            },
+            {
+                // https://report-uri.com/home/generate
+                key: 'Content-Security-Policy',
+                value: `default-src 'self'; script-src 'self'; script-src-elem 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self'`
+            }
+        );
+    }
+
+    return headers;
+}
+
+const isProd = process.env['NODE_ENV'] === 'production';
+
 module.exports = {
     // add environment variables accessible via AppSettings here:
     // visible only by server-side Next.js (secrets)
@@ -10,5 +54,14 @@ module.exports = {
         oidcScope: process.env['OIDC_SCOPE'],
     },
     productionBrowserSourceMaps: true,
-    swcMinify: true
+    swcMinify: true,
+    async headers() {
+        return [
+            {
+                // Apply these headers to all routes in your application.
+                source: '/:path*',
+                headers: getSecurityHeaders(isProd),
+            },
+        ]
+    },
 }

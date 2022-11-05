fb6a834b958c30442d083db0a2edeec9f1bfa4ac
Ryan Elian <ryan.elian.cs@gmail.com>
2022-11-05 09:25:16 +0700

    Update stack to Next.js 13 + Node 18 LTS

--- next.config.js
+++ next.config.js
@@ -52,26 +52,6 @@ const nextConfig = {
     // https://nextjs.org/docs/basic-features/data-fetching/get-server-side-props
     serverRuntimeConfig: require('./appsettings'),
     productionBrowserSourceMaps: true,
-    /*
-        Disable swcMinify for now
-        https://github.com/vercel/next.js/issues/40889
-        https://github.com/ant-design/ant-design/issues/38106
-        
-        index.js:235 
-        
-        TypeError: number 0 is not iterable (cannot read property Symbol(Symbol.iterator))
-        at new Map (<anonymous>)
-        at Overflow.js:173:1
-        at useBatchFrameState.js:32:34
-        at eD (Overflow.js:173:1)
-        at P (Item.js:33:1)
-        at Item.js:38:1
-        at uJ (react-dom.production.min.js:241:246)
-        at u1 (react-dom.production.min.js:245:114)
-        at o5 (react-dom.production.min.js:285:374)
-        at react-dom.production.min.js:284:398
-    */
-    swcMinify: false,
     async headers() {
         return [
             {
